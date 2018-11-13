aae3e403ca2f8a0a81b1e3bf749a4b44ee84a398
Joseph Lionarons <lionsamurai@gmail.com>
2018-11-13 18:49:05 -0500

    basic express routing

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 const express = require('express');
-const router = express.Router();
+const app = express();
+const morgan = require('morgan');
+const router = require('./routes/index');
+const path = require('path');
+const PORT = 2500;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,56 @@
 const express = require('express');
-const app = express();
-const morgan = require('morgan');
+const router = express.Router();
 
-app.use(morgan('dev'));
+router.get('/', (req, res, next) => {
+  res.status(200).sendFile('../public/index.html');
+});
 
+router.get('/acro', (req, res, next) => {
+  res.status(200).sendFile('../public/acro.html');
+});
 
-module.exports = app;
+router.get('/contact', (req, res, next) => {
+  res.status(200).sendFile('../public/contact/html');
+});
+
+router.get('/dance', (req, res, next) => {
+  res.status(200).sendFile('../public/dance.html');
+});
+
+router.get('/events', (req, res, next) => {
+  res.status(200).sendFile('../public/events.html');
+});
+
+router.get('/faculty', (req, res, next) => {
+  res.status(200).sendFile('../public/faculty.html');
+});
+
+router.get('/faq', (req, res, next) => {
+  res.status(200).sendFile('../public/faq.html');
+});
+
+router.get('/gallery', (req, res, next) => {
+  res.status(200).sendFile('../public/gallery.html');
+});
+
+router.get('/karate', (req, res, next) => {
+  res.status(200).sendFile('../public/karate.html');
+});
+
+router.get('/music', (req, res, next) => {
+  res.status(200).sendFile('../public/music.html');
+});
+
+router.get('/programs', (req, res, next) => {
+  res.status(200).sendFile('../public/programs.html');
+});
+
+router.get('/schedule', (req, res, next) => {
+  res.status(200).sendFile('../public/schedule.html');
+});
+
+router.get('/tuition', (req, res, next) => {
+  res.status(200).sendFile('../public/tuition.html');
+});
+
+module.exports = router;
