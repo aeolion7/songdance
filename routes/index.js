bf16c7cddfcf6b4629153d1ff1f90f395fe37199
Joseph Lionarons <lionsamurai@gmail.com>
2018-11-13 20:18:19 -0500

    css and routing changes

--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,68 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 router.get('/', (req, res, next) => {
   res.status(200).sendFile('../public/index.html');
 });
 
 router.get('/acro', (req, res, next) => {
-  res.status(200).sendFile('../public/acro.html');
+  res.status(200).sendFile(path.join(__dirname, '../public', 'acro.html'));
 });
 
 router.get('/contact', (req, res, next) => {
-  res.status(200).sendFile('../public/contact/html');
+  res.status(200).sendFile(path.join(__dirname, '../public', 'contact.html'));
 });
 
 router.get('/dance', (req, res, next) => {
-  res.status(200).sendFile('../public/dance.html');
+  res.status(200).sendFile(path.join(__dirname, '../public', 'dance.html'));
 });
 
 router.get('/events', (req, res, next) => {
-  res.status(200).sendFile('../public/events.html');
+  res.status(200).sendFile(path.join(__dirname, '../public', 'events.html'));
 });
 
 router.get('/faculty', (req, res, next) => {
-  res.status(200).sendFile('../public/faculty.html');
+  res.status(200).sendFile(path.join(__dirname, '../public', 'faculty.html'));
 });
 
 router.get('/faq', (req, res, next) => {
-  res.status(200).sendFile('../public/faq.html');
+  res.status(200).sendFile(path.join(__dirname, '../public', 'faq.html'));
 });
 
 router.get('/gallery', (req, res, next) => {
-  res.status(200).sendFile('../public/gallery.html');
+  res.status(200).sendFile(path.join(__dirname, '../public', 'gallery.html'));
 });
 
 router.get('/karate', (req, res, next) => {
-  res.status(200).sendFile('../public/karate.html');
+  res.status(200).sendFile(path.join(__dirname, '../public', 'karate.html'));
 });
 
 router.get('/music', (req, res, next) => {
-  res.status(200).sendFile('../public/music.html');
+  res.status(200).sendFile(path.join(__dirname, '../public', 'music.html'));
 });
 
 router.get('/programs', (req, res, next) => {
-  res.status(200).sendFile('../public/programs.html');
+  res.status(200).sendFile(path.join(__dirname, '../public', 'programs.html'));
 });
 
 router.get('/schedule', (req, res, next) => {
-  res.status(200).sendFile('../public/schedule.html');
+  res.status(200).sendFile(path.join(__dirname, '../public', 'schedule.html'));
 });
 
 router.get('/tuition', (req, res, next) => {
-  res.status(200).sendFile('../public/tuition.html');
+  res.status(200).sendFile(path.join(__dirname, '../public', 'tuition.html'));
+});
+
+// TODO: create custom 404 page
+router.use((req, res, next) => {
+  res.status(404).send('Page Not Found');
+});
+
+// TODO: create custom 500 page
+router.use((err, req, res, next) => {
+  console.error(err.message);
+  res.status(500).send('Server Error');
 });
 
 module.exports = router;
